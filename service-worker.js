19d561fa2727dad1cc1d6f094272590ec127268d
David <davidadesola2023@gmail.com>
2025-01-19 21:22:12 +0000

    second-commit

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,19 @@
 const CACHE_NAME = 'portfolio-cache-v1';
-const urlsToCache = ['/', '/index.html', '/style.css', '/manifest.json'];
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/about.html',
+    '/projects.html',
+    '/works.html',
+    '/manifest.json',
+    '/coach_two.jpg',
+    '/cv.jpg',
+    '/game.png',
+    '/wallpapersden.com_programming-8k-cool_600x600.jpg',
+    '/wallpapersden.com_stargazing-hd-pixel-art_600x600.jpg',
+    '/ww.jpg',
+    '/your-picture.jpeg'
+];
 
 self.addEventListener('install', event => {
     event.waitUntil(
